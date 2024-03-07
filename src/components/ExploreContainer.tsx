e61502fc037003912167bca26380a0f126300d24
FatimaPaluga13 <palugafatima1@gmail.com>
2024-03-07 12:39:25 +0800

    addeed card component on homepage

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,13 +1,14 @@
 import './ExploreContainer.css';
-
+import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <IonCard>
     <IonCardHeader>
-      <IonCardTitle>Card Title</IonCardTitle>
-      <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      <IonCardTitle>Hello World</IonCardTitle>
+      <IonCardSubtitle>This is My Ionic Application</IonCardSubtitle>
     </IonCardHeader>
 
     <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
